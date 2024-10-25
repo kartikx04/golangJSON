29ead22e70c1a89b5059d0cace888fe854ac874b
Siddharth <kartikx04@gmail.com>
2024-10-25 15:12:19 +0530

    added a function called write() that creates file with a text in it.

--- defer/main.go
+++ defer/main.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	defer fmt.Println("Understanding defer")
+	if err := write("readme.txt", "Hi there, welcome to golang project"); err != nil {
+		log.Fatal(err)
+	}
+}
 
-	fmt.Println("Project started")
+func write(filename, text string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.WriteString(file, text)
+	if err != nil {
+		return err
+	}
+	file.Close()
+	return nil
 }
