a52d7912f20e5bd250b373ddfa331d0d4e184225
Siddharth <kartikx04@gmail.com>
2024-11-01 18:58:29 +0530

    created function doSomething to fetch value to doAnother using channel until ctx.Done() is fetched

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
+func generateNumbers(total int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i <= total; i++ {
+		fmt.Printf("Generated Number: %d\n", i)
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func printNumbers(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i <= 20; i++ {
+		fmt.Printf("Printed Number: %d\n", i)
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
 func main() {
-	fmt.Println("Hi")
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go printNumbers(&wg)
+	go generateNumbers(20, &wg)
+
+	wg.Wait()
 }
--- context/main.go
+++ context/main.go
@@ -3,15 +3,44 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 func doSomething(ctx context.Context) {
-	fmt.Printf("We changed Key value to %s.\n", ctx.Value("Key"))
+	ctx, cancelCtx := context.WithCancel(ctx)
+	printCh := make(chan int)
+
+	go doAnother(ctx, printCh)
+
+	for i := 0; i <= 3; i++ {
+		printCh <- i
+	}
+
+	cancelCtx()
+
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Printf("doSomething finished\n")
+
+}
+
+func doAnother(ctx context.Context, printCh <-chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			if err := ctx.Err(); err != nil {
+				fmt.Printf("doAnother error %s\n", err)
+			}
+			fmt.Printf("doAnother finished\n")
+			return
+		case num := <-printCh:
+			fmt.Printf("doAnother : %d\n", num)
+		}
+	}
 }
 
 func main() {
 	ctx := context.TODO()
 
-	ctx = context.WithValue(ctx, "Key", "KeyValue")
 	doSomething(ctx)
 }
