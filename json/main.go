fa01d06d706c130f95ad095d328644dc97265ea6
Siddharth <kartikx04@gmail.com>
2024-11-01 19:11:21 +0530

    marshalled go data inro json data

--- json/main.go
+++ json/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
 func main() {
-	fmt.Printf("Hi")
+	data := map[string]interface{}{
+		"intValue":    1234,
+		"boolValue":   true,
+		"stringValue": "hello!",
+		"objectValue": map[string]interface{}{
+			"arrayValue": []int{1, 2, 3, 4},
+		},
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("error occured : %s", err)
+	}
+
+	fmt.Printf("Formatted data : %s", jsonData)
 }
